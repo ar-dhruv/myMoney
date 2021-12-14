df7be19c1881bf832a2467a6caf399a5ec168a53
Dhruv <dev.aroradhruv@gmail.com>
2021-12-14 08:36:57 +0530

    setting states for login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
+//STYLES
 import styles from "./Login.module.css";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <form className={styles["login-form"]}>
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
       <h2>Login</h2>
       <label>
         <span>email:</span>
-        <input type="email" />
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email} //CHANGING THE INPUT TEXT TO THE UPDATED STATE VALUE
+        />
       </label>
       <label>
         <span>password:</span>
-        <input type="password" />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password} //CHANGING THE INPUT TEXT TO THE UPDATED STATE VALUE
+        />
       </label>
       <button className="btn">Login</button>
     </form>
