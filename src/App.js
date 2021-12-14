7a4dba862beff3f44ffa3a3b4fc7a46069d23fcc
Dhruv <dev.aroradhruv@gmail.com>
2021-12-14 08:07:20 +0530

    creating pages & setting routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//PAGES & COMPONENTS
+import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
