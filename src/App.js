80806a4b7eee3afa60eaf14c5d84d3bc94a7df3c
Dhruv <dev.aroradhruv@gmail.com>
2021-12-14 08:16:24 +0530

    Navbar component added and styled

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
