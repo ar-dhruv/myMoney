4d3b046aefea10c4ebb7559e62b77bbb65b5a0b2
Dhruv <dev.aroradhruv@gmail.com>
2021-12-17 13:33:28 +0530

    using the logout hook in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 //STYLES
 import styles from "./Navbar.module.css";
 export default function Navbar() {
+  const { logout } = useLogout(); //DESTRUCTURING THE LOGOUT FUNCTION FROM USELOGOUT HOOK
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -12,6 +15,11 @@ export default function Navbar() {
         </li>
         <li>
           <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
