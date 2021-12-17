0fbf630dde6675ffedb8bf79c8f2f6e08c563368
Dhruv <dev.aroradhruv@gmail.com>
2021-12-17 16:22:36 +0530

    Conditional buttons in the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,40 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //STYLES
 import styles from "./Navbar.module.css";
 export default function Navbar() {
   const { logout } = useLogout(); //DESTRUCTURING THE LOGOUT FUNCTION FROM USELOGOUT HOOK
+  const { user } = useAuthContext(); //DESTRUCTURING THE USER PROPERTY FROM USEAUTHCONTEXT HOOK
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+
+        {/* IF WE DONT HAVE A USER THEN ONLY WE SHOW THE LOGIN & SIGNUP LINKS*/}
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {/* IF WE USER IF ALREADY IN THE STATE THEN WE WILL ONLY SHOW THE LOGOUT BITTON */}
+        {user && (
+          <>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
