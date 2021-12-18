1ad28fde94174d0bec6f51beb3b5cacdc0153cf5
Dhruv <dev.aroradhruv@gmail.com>
2021-12-18 16:34:52 +0530

    creating transactionList component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,6 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionList from "./TransactionList";
 
 //STYLES
 import styles from "./Home.module.css";
@@ -7,10 +9,14 @@ import styles from "./Home.module.css";
 import TransactionForm from "./TransactionForm";
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions");
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction List</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
