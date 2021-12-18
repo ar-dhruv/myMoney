4c8dddb4b1dbd43b2cdbfcd4005d035d5f3083e6
Dhruv <dev.aroradhruv@gmail.com>
2021-12-18 08:32:18 +0530

    completing useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -8,7 +8,7 @@ let initialState = {
   success: null,
 };
 
-const firstoreReducer = (state, action) => {
+const firestoreReducer = (state, action) => {
   switch (action.type) {
     default:
       return state;
@@ -17,4 +17,21 @@ const firstoreReducer = (state, action) => {
 
 export const useFirestore = (collection) => {
   const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false); //STATE USED FOR CLEANUP FUNCTION...SET TRUE WHEN THE COMPONENT USE THIS HOOK UNMOUNTS
+
+  //COLLECTION REFERENCE
+  const ref = projectFirestore.collection(collection);
+
+  // ADD DOCUMENT
+  const addDocument = (doc) => {};
+
+  // DELETE DOCUMENT
+  const deleteDocument = (id) => {};
+
+  //CLEAN-UP FUNCTION
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
 };
