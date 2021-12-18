78c816c91b5beefcc099435905560220575d111a
Dhruv <dev.aroradhruv@gmail.com>
2021-12-18 08:00:41 +0530

    transaction form created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
-// import styles from "./Home.module.css";
-export default function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+//STYLES
+import styles from "./Home.module.css";
 
+//COMPONENTS
+import TransactionForm from "./TransactionForm";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transactiion List</div>
+      <div className={styles.sidebar}>
+          <TransactionForm/>
+      </div>
+    </div>
+  );
+}
