5646869597ccd59b63b3dacea7be3ff73fcabdba
Dhruv <dev.aroradhruv@gmail.com>
2021-12-18 12:36:10 +0530

    Adding a document in the firestore collection

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,8 +10,29 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-    case "IS_PENDING":
-      return { ...state, isPending: true };
+    case "IS_PENDING": //THE LOADING CASE , STTING THE ISPENDING TO BE TRUE WITH CURRENT STATE PROPERTIES
+      return {
+        isPending: true,
+        document: null,
+        success: false,
+        error: null,
+      };
+
+    case "ADDED_DOCUMENT": //ADDING DOCUMENT CASE , WE SUCCESSFULLY ADDED A DOCUMENT HENCE THE FOLLOWING STATE PROPERTY CHANGES
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+
+    case "ERROR": //ERROR STATE , SETTING ERROR TO BE ACTION.PAYLOAD i.e THE ERROR MEASSAGE IN THE CATCH BLOCK
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
 
     default:
       return state;
@@ -25,26 +46,26 @@ export const useFirestore = (collection) => {
   //COLLECTION REFERENCE
   const ref = projectFirestore.collection(collection);
 
-
   //ONLY DISPATCH IF THE COMPONENT IS STILL MOUNTED i.e ISCANCELLED IS FALSE
   const dispatchIFNotCancelled = (action) => {
-    if(!isCancelled){
-      dispatch(action)
+    if (!isCancelled) {
+      dispatch(action);
     }
-  }
+  };
 
   // ADD DOCUMENT
   const addDocument = async (doc) => {
     dispatch({ type: "IS_PENDING" });
 
-
     //TRING TO ADD A NEW DOCUMENT
-    try{
-      const addedDocument = await ref.add(doc)
-      
-    }
-    catch(err){
-
+    try {
+      const addedDocument = await ref.add(doc);
+      dispatchIFNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (err) {
+      dispatchIFNotCancelled({ type: "ERROR", payload: err.message });
     }
   };
 
