5eaca35146e81cb98e316b6c612179a4844af08c
Dhruv <dev.aroradhruv@gmail.com>
2021-12-19 07:55:53 +0530

    ordering the documents in the transaction list

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,18 +1,23 @@
 import { useEffect, useState, useRef } from "react";
 import { projectFirestore } from "../firbase/config";
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query , _orderBy) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   //IF WE DON'T USE A REF THEN INFINITE LOOP IN USEEFFECT AS QUERY IS AN ARRAY & IS DIFFERENT ON EVERY FUNCTION CALL BECAUSE ITS REFERENCE IS CHANGED IN THE MEMORY EVERYTIME SO USEEFECT SEES IT AS A DIFFERENT ARRAY EVERYTIME SO WE USE A USEREF HOOK
   const query = useRef(_query).current;
+  const orderBy = useRef(_orderBy).current;
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection); //THIS USEEFFECT WILL FIRE AS SOON AS THE COMPONENT USING THIS USECOLLECTION HOOK MOUNTS & HENCE FETCHING THE COLLECTION DATA FROM THE FIRESTORE
 
     if (query) {
-      ref = ref.where(...query); //FOR ONLY FETCHING DATA OF THE USER HAVING USER.ID == UID....PASSED AS A PROP FROM THE HOME PAGE COMPONENT
+      ref = ref.where(...query); //FOR ONLY FETCHING DATA OF THE USER HAVING USER.ID == UID....PASSED AS A PROP FROM THE HOME PAGE COMPONENT IN FORM OF AN ARRAY
+    }
+
+    if(orderBy){
+      ref = ref.orderBy(...orderBy); //FOR ORDERING THE DOCUMENTS IN THE TRANSACTION LIST BASED ON THE PROPERTY CREATEDAT & IN DESC ORDER BOTH PASSED AS A PROPS FROM THE HOME COMPONENT IN FORM OF AN ARRAY
     }
 
     const unsubscribe = ref.onSnapshot(
@@ -40,7 +45,7 @@ export const useCollection = (collection, _query) => {
 
     //UNSUBSCRIBE ON UNMOUNTING THE COMPONENT WHICH IS USING THIS HOOK
     return () => unsubscribe();
-  }, [collection, query]);
+  }, [collection, query , orderBy]);
 
   return { documents, error };
 };
